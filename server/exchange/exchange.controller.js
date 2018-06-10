63d8bb12127c942e287c218b4875c8155f992572
xchao <xchao0213@gmail.com>
2018-06-10 20:49:20 +0800

    exchangeinfo with volume

--- server/exchange/exchange.controller.js
+++ server/exchange/exchange.controller.js
@@ -1,23 +1,67 @@
+const util = require('../util');
 const db = require('../../config/mysql');
-const util = require('../util')
+const exchangeDb = require('./exchange.db');
 
+/**
+ * @api {get} /api/exchange/ Request exchange list
+ * @apiName GetExchangeList
+ * @apiGroup exchange
+ * @apiParam {number} res.query.start Start of list
+ * @apiParam {number} res.query.count Count of exchanges
+ * @apiParam {number} res.query.limit Limit of sql query volume
+ * @apiSuccess {json} Array of Exchanges info
+ */
 function list(req, res, next) {
     let start = req.query.start;
     let count = req.query.count;
-    db.query('select * from exchangeinfo limit ' + start + ',' + count, function (err, results, fields) {
-        if (err) throw err;
-        res.json(results);
+    let limit = req.query.limit;
+    exchangeDb.queryList(start, count).then(results => {
+        return Promise.all(results.map(exchange => {
+            return bundleVolume(exchange, limit)
+        }))
+        
+    }).then(all => {
+        res.json(all)
+    }).catch(err => {
+        throw err;
+    });
+}
+
+/**
+ * bundle exchange info
+ * @param {*} exchange 
+ * @param {*} limit 
+ */
+function bundleVolume(exchange,limit) {
+    return new Promise((resolve, reject) => {
+        exchangeDb.queryVolume(exchange.name, limit).then(res => {
+            let volume = util.convert(res);
+            exchange.volume = volume;
+            resolve(exchange);
+        }).catch(err => {
+            reject(err);
+        })
     })
 }
 
 function load(req, res, next, name) {
-    db.query("select volume from exchange where exchange.name = '" + name + "' order by `timestamp` DESC limit 100", function (err, results, fields) {
-        if (err) throw err;
+    let limit = req.query.limit;
+    exchangeDb.queryVolume(name, limit).then(results => {
         req.results = util.convert(results);
         return next();
-    })
+    }).catch(err => {
+        throw err;
+    });
+
 }
 
+/**
+ * @api {get} /api/exchange/:name Request exchange info
+ * @apiName GetExchangeInfo
+ * @apiGroup exchange
+ * @apiParam {string} :name Name of exchange
+ * @apiSuccess {json} Json of exchange info
+ */
 function get(req, res) {
     return res.json(req.results);
 }
---
+++ server/exchange/exchange.db.js
@@ -0,0 +1,23 @@
+const db = require('../../config/mysql');
+
+function queryList(start, count) {
+    return new Promise((resolve, reject) => {
+        db.query('select * from exchangeinfo where alive=True order by `rank` limit ' + start + ',' + count, function (err, results, fields) {
+            if (err)
+                return reject(err);
+            resolve(results);
+        })
+    })
+
+}
+
+function queryVolume(name, limit) {
+    return new Promise((resolve, reject) => {
+        db.query("select volume from exchange where exchange.name = '" + name + "' order by `timestamp` DESC limit " + limit, function (err, results, fields) {
+            if (err) reject(err);
+            resolve(results);
+        })
+    })
+
+}
+module.exports = { queryList, queryVolume };
